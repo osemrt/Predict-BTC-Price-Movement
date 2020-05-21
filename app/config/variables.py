43bbc090d23c3c8b38235c45eddf4cd99d269294
Mert ÖZ <osemrt@gmail.com>
2020-05-21 08:34:07 +0300

    Add Config file

---
+++ app/config/variables.py
@@ -0,0 +1,22 @@
+CNN_LSTM = 3
+SENTIMENT_LSTM = 4
+
+HISTORICAL_DATASET_PATH = "../models/sentiment-lstm/combined_data.csv"
+CNN_LSTM_MODEL_PATH = "../models/cnn-lstm/model-0.6238937973976135.h5"
+SENTIMENT_LSTM_MODEL_PATH = "../models/sentiment-lstm/sentiment-lstm.h5"
+SENTIMENT_LSTM_MODEL_3_MONTHS_PATH = "../models/sentiment-lstm/experiments/00_last_three_months/sentiment-lstm.h5"
+SENTIMENT_LSTM_MODEL_6_MONTHS_PATH = "../models/sentiment-lstm/experiments/01_last_six_months/sentiment-lstm.h5"
+SENTIMENT_LSTM_MODEL_LAST_YEAR_PATH = "../models/sentiment-lstm/experiments/02_last_one_year/sentiment-lstm.h5"
+
+
+INITIAL_BTC_PRICE = 5095.76
+
+MESSAGE_DAILY_STATUS = 0
+MESSAGE_RESULT = 1
+MESSAGE_PERIOD = 2
+MESSAGE_AVERAGE = 3
+MESSAGE_COUNT = 4
+MESSAGE_DEFAULT = 5
+
+DECREASE = 0
+INCREASE = 1
